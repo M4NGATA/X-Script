00e93b8e4427994f9c4f5a12a2739d05ae05deb8
M4NGATA <m4ngata@tuta.io>
2023-12-01 18:26:45 +0300

    update

---
+++ Function/server/server.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Подгрузка общих функций и цвета
+	clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Common/theme.sh) && printlogo
+# Шапка скрипта
+	echo "$(printBMagenta ' СЕРВЕР')"
+# Основное меню
+	mainmenu() {
+		echo "$(printBGreen ' 1 ')Системный монитор"
+		echo "$(printBGreen ' 2 ')Настройка прокси"
+		echo "$(printBGreen ' 3 ')Настройка портов"
+		echo "$(printBGreen ' 4 ')VPN"
+		echo ' --------'
+		echo "$(printBRed ' 0 ')Назад"
+		echo "$(printBRed ' 10 ')Выход"
+		echo ' --------'
+		echo -ne "$(printBGreen ' Ввод')$(printGreenBlink ':')"
+#	Свойства меню
+	read -r ans
+		case $ans in
+
+			1)
+			source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Function/server/sm.sh
+			;;
+
+			2)
+			source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Function/server/dante_proxy.sh
+			;;
+
+			3)
+			source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Function/server/ports.sh
+			;;
+
+			4)
+			clear && printlogo && echo "$(printBYellow ' Coming soon!')" && mainmenu
+			;;
+
+			0)
+			source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Menu/start.sh)
+			;;
+			
+			10)
+			echo $(printBCyan '"Bye bye."')
+			exit
+			;;
+			
+			*)
+			clear && printlogo && echo "$(printBRed ' Неверный запрос!')" && mainmenu
+			;;
+
+	esac
+}
+
+
+mainmenu
---
+++ Function/server/sm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Подгрузка общих функций и цвета
+	clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Common/theme.sh) && printlogo
+
+
+# Проверка наличия установленного пакета btop через snap
+	if ! snap list | grep -q btop; then
+		echo "Установка пакета btop..."
+		sudo apt install snapd -y && snap install btop
+		echo "Пакет btop установлен."
+fi
+
+# Запуск btop
+echo "Запуск btop..."
+snap run btop
+
+source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Function/server/server.sh)
