495cdbebf5d568b516fa4a0a6c1c4db437d0f597
M4NGATA <m4ngata@tuta.io>
2023-12-20 14:05:26 +0300

    update

---
+++ Common/printlogo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+printlogo() {
+    local color='\033[1;32m'  # Цвет текста - темно-зеленый
+    local reset='\033[0m'  # Сброс цвета
+
+#     echo -e "${color}""  ┏┓┏┓  ┓┓┓     "
+#     echo "   ┃┃ ━━┃┃┣┓┏┓┏┓"
+#     echo -e "  ┗┛┗┛  ┗┻┗┛┛ ┗┻  "" ${reset}"
+# }
+
+
+ echo -e "${color}""  -❄-❄-❄-❄-❄-❄-❄-"
+ echo "      X-l1bra"
+ echo -e "${color}""  -❄-❄-❄-❄-❄-❄-❄-"" ${reset}"
+
+}
+
+printlogo
