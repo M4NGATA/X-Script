286bb3dbdbcd1dc0234ee14c248584a646bb80d4
M4NGATA <143332616+M4NGATA@users.noreply.github.com>
2024-05-10 21:32:59 +0300

    Update main.sh

--- Projects/Taiko/main.sh
+++ Projects/Taiko/main.sh
@@ -4,11 +4,7 @@ echo -e "\e[1;35m
 ╔╦╗╔═╗╦╦╔═╔═╗
  ║ ╠═╣║╠╩╗║ ║
  ╩ ╩ ╩╩╩ ╩╚═╝\e[0m"
-
-    echo " 1. Управление"
-    echo " 2. Установка"
-    echo " 3. Обновление"
-    echo " 4. Удаление"
+    echo " 1. Устарновка Hekla"
     echo " ---------"
     echo "  0. Назад"
     echo " 10. Выход"
@@ -17,10 +13,7 @@ echo -e "\e[1;35m
     read -r ans
 
     case $ans in
-        1) clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Projects/Taiko/control.sh) ;;
-        2) clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Projects/Taiko/install.sh) ;;
-        4) clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Projects/Taiko/delete.sh) ;;
-        3) clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Projects/Taiko/update.sh) ;;
+        1) clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Projects/Taiko/install_Hekla.sh) ;;
         0) clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Menu/menu_nodes.sh) ;;
         10) echo "До свидания." && exit ;;
         *) clear && echo "Неверный ввод!" && mainmenu ;;
