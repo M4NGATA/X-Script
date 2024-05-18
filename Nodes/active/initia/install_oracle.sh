2bd137295240e4029c36e259b0b9129ca8c850c7
M4NGATA <143332616+M4NGATA@users.noreply.github.com>
2024-05-18 13:02:35 +0300

    Create install_oracle.sh

---
+++ Nodes/active/initia/install_oracle.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+echo
+read -p "Вы уверены что хотите начать установку Oracle? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+# Clone repository
+cd $HOME
+rm -rf slinky
+git clone https://github.com/skip-mev/slinky.git
+cd slinky
+git checkout v0.4.3
+
+# Build binaries
+make build
+
+# Move binary to local bin
+mv build/slinky /usr/local/bin/
+rm -rf build
+
+sudo tee /etc/systemd/system/slinky.service > /dev/null <<EOF
+[Unit]
+Description=Initia Slinky Oracle
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which slinky) --oracle-config-path $HOME/slinky/config/core/oracle.json --market-map-endpoint 127.0.0.1:17990
+Restart=on-failure
+RestartSec=30
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable slinky.service
+sudo systemctl start slinky.service
+
+read -p "Установка завершена для выхода в меню нажмите Enter"
+
+source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Nodes/active/initia/main.sh)
+
